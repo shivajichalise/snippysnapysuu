91399f848c9a6ee913212f9fd204f458b0ac7e73
Shivaji Chalise <respond2shivaji@gmail.com>
2024-02-27 19:19:52 +0545

    few fixes

--- frontend/src/components/InputSelect.tsx
+++ frontend/src/components/InputSelect.tsx
@@ -1,29 +1,50 @@
 import { forwardRef } from "react"
 import InputSelectProps from "../types/InputSelectProps"
 
-const InputSelect = forwardRef<HTMLSelectElement, InputSelectProps>((props, ref) => {
-    const {id, name, placeholder, onChange, hasLabel, inputLabel, required, options} = props
+const InputSelect = forwardRef<HTMLSelectElement, InputSelectProps>(
+    (props, ref) => {
+        const {
+            id,
+            name,
+            placeholder,
+            onChange,
+            hasLabel,
+            inputLabel,
+            required,
+            options,
+        } = props
 
-    return (
-        <div className="w-full">
-            {hasLabel && (
-                <div className="m-1">
-                    <label className="text-xs">{inputLabel ?? name.toUpperCase()} {required && <span className="text-error-100 text-md">*</span>}</label>
-                </div>
-            )}
-            <select
-                className='focus:border-primary-200 text-text-200 bg-300 border-100 block w-full rounded-lg border p-2.5 text-sm placeholder-gray-400 outline-none'
-                id={id}
-                name={name}
-                placeholder={placeholder}
-                onChange={onChange}
-                ref={ref}
-            >
-                { options.map(op => <option value={op.value}>{op.label}</option>) }
-
-            </select>
-        </div>
-    )
-})
+        return (
+            <div className="w-full">
+                {hasLabel && (
+                    <div className="m-1">
+                        <label className="text-xs">
+                            {inputLabel ?? name.toUpperCase()}{" "}
+                            {required && (
+                                <span className="text-error-100 text-md">
+                                    *
+                                </span>
+                            )}
+                        </label>
+                    </div>
+                )}
+                <select
+                    className="focus:border-primary-200 text-text-200 bg-300 border-100 block w-full rounded-lg border p-2.5 text-sm placeholder-gray-400 outline-none"
+                    id={id}
+                    name={name}
+                    placeholder={placeholder}
+                    onChange={onChange}
+                    ref={ref}
+                >
+                    {options.map((op) => (
+                        <option key={op.value} value={op.value}>
+                            {op.label}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        )
+    }
+)
 
 export default InputSelect
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,73 +1,84 @@
 import {
-  IconFolderFilled,
-  IconPlus,
-  IconSourceCode,
-  IconStarsFilled,
-  IconTag,
+    IconFolderFilled,
+    IconPlus,
+    IconSourceCode,
+    IconStarsFilled,
+    IconTag,
 } from "@tabler/icons-react"
 import Anchor from "./Anchor"
 import Button from "./Button"
 import collections from "../config/collections"
 import tags from "../config/tags"
 import SidebarProps from "../types/SidebarProps"
+import IconButton from "./IconButton"
 
 const Sidebar = (props: SidebarProps) => {
-  return (
-    <div className='border-200 bg-300 flex h-[calc(100vh-(3.5rem))] w-64 flex-col border-r p-4'>
-      <div className='mb-4'>
-        <Anchor
-          to='/home'
-          icon={<IconSourceCode size={16} />}
-          text='Snippets'
-          isActive={props.tab === "snippets" ? true : false}
-          select={() => props.selectTab("snippets", "snippets")}
-        />
-        <Anchor
-          to='#'
-          icon={<IconStarsFilled size={16} />}
-          text='Favourites'
-          isActive={props.tab === "favourites" ? true : false}
-          select={() => props.selectTab("favourites", "favourites")}
-        />
-      </div>
-      <div className='mb-4'>
-        <div className='flex items-center justify-between'>
-          <label className='text-accent-200 text-sm'>Collections</label>
-          <Button type='primary' onClick={() => console.log("new collectin")}>
-            <IconPlus size={13} />
-          </Button>
+    return (
+        <div className="border-200 bg-300 flex h-[calc(100vh-(3.5rem))] w-64 flex-col border-r p-4">
+            <div className="mb-4">
+                <Anchor
+                    to="/home"
+                    icon={<IconSourceCode size={16} />}
+                    text="Snippets"
+                    isActive={props.tab === "snippets" ? true : false}
+                    select={() => props.selectTab("snippets", "snippets")}
+                />
+                <Anchor
+                    to="#"
+                    icon={<IconStarsFilled size={16} />}
+                    text="Favourites"
+                    isActive={props.tab === "favourites" ? true : false}
+                    select={() => props.selectTab("favourites", "favourites")}
+                />
+            </div>
+            <div className="mb-4">
+                <div className="flex items-center justify-between">
+                    <label className="text-accent-200 text-sm">
+                        Collections
+                    </label>
+                    <IconButton
+                        type="primary"
+                        onClick={() => console.log("new collectin")}
+                    >
+                        <IconPlus size={13} />
+                    </IconButton>
+                </div>
+                {collections.map((collection) => (
+                    <Anchor
+                        key={collection.id}
+                        to="#"
+                        icon={<IconFolderFilled size={16} />}
+                        text={collection.name}
+                        isActive={props.tab === collection.slug ? true : false}
+                        select={() =>
+                            props.selectTab(collection.slug, "collection")
+                        }
+                    />
+                ))}
+            </div>
+            <div className="mb-4">
+                <div className="flex items-center justify-between">
+                    <label className="text-accent-200 text-sm">Tags</label>
+                    <IconButton
+                        type="primary"
+                        onClick={() => console.log("new tag")}
+                    >
+                        <IconPlus size={13} />
+                    </IconButton>
+                </div>
+                {tags.map((tag) => (
+                    <Anchor
+                        key={tag.id}
+                        to="#"
+                        icon={<IconTag size={16} />}
+                        text={tag.name}
+                        isActive={props.tab === tag.slug ? true : false}
+                        select={() => props.selectTab(tag.slug, "tag")}
+                    />
+                ))}
+            </div>
         </div>
-        {collections.map((collection) => (
-          <Anchor
-            key={collection.id}
-            to='#'
-            icon={<IconFolderFilled size={16} />}
-            text={collection.name}
-            isActive={props.tab === collection.slug ? true : false}
-            select={() => props.selectTab(collection.slug, "collection")}
-          />
-        ))}
-      </div>
-      <div className='mb-4'>
-        <div className='flex items-center justify-between'>
-          <label className='text-accent-200 text-sm'>Tags</label>
-          <Button type='primary' onClick={() => console.log("new Tag")}>
-            <IconPlus size={13} />
-          </Button>
-        </div>
-        {tags.map((tag) => (
-          <Anchor
-            key={tag.id}
-            to='#'
-            icon={<IconTag size={16} />}
-            text={tag.name}
-            isActive={props.tab === tag.slug ? true : false}
-            select={() => props.selectTab(tag.slug, "tag")}
-          />
-        ))}
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Sidebar
