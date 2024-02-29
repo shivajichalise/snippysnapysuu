3f25038a2ce1efed311041734dbcf871f781ae42
Shivaji Chalise <respond2shivaji@gmail.com>
2024-02-29 21:28:17 +0545

    adding snippet route

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import "dotenv/config"
 import express from "express"
-import authRoutes from './routes/authRoutes'
+import authRoutes from "./routes/authRoutes"
+import snippetRoutes from "./routes/snippetRoutes"
 import { success } from "./utils/httpResponses"
 import HttpResponsesParams from "./types/HttpResponsesParams"
 import bodyParser from "body-parser"
@@ -10,27 +11,30 @@ import setupDB from "./config/setupDB"
 
 const app = express()
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-app.use(cors({
-    credentials: true,
-    origin: "http://localhost:5173",
-}))
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:5173",
+    })
+)
 
 setupDB()
 
-app.get('/api', (_, res) => {
+app.get("/api", (_, res) => {
     const params: HttpResponsesParams<[]> = {
         res: res,
         data: [],
         message: "Hello World",
-        code: 200
+        code: 200,
     }
     success(params)
 })
 
-app.use('/api/auth', authRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/snippets", snippetRoutes)
 
 export default app
